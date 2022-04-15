b6c99d72e60c155ac85bc695250ef12bcc960229
amasgari <amasgari@ce.sharif.edu>
2022-04-15 21:56:56 +0430

    Modify UserTest.java to improve coverage percent

--- src/test/java/usermanagement/service/UserTest.java
+++ src/test/java/usermanagement/service/UserTest.java
@@ -1,4 +1,25 @@
 package usermanagement.service;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class UserTest {
+
+    @Test
+    public void test_equality() {
+        User user1 = new User();
+        user1.setFirstName("f");
+        user1.setLastName("l");
+
+        User user2 = new User();
+        user2.setFirstName("f");
+        user2.setLastName("l");
+
+
+        assertTrue(user1.equals(user2));
+
+    }
 }
+
